e029538dcdac8a0f78def3e46729cfa91a1a50f0
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-17 17:42:50 +0700

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON", "").strip()
-DRIVE_FOLDER_ID = os.getenv("DRIVE_FOLDER_ID", "").strip()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "").strip()
