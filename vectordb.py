e480e4b797a9537ba6dd178352c9c7827129fab2
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-19 11:13:26 +0700

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -1,17 +1,12 @@
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 
-from config import (
-    QDRANT_URL,
-    QDRANT_API_KEY,
-    QDRANT_COLLECTION,
-)
+from config import QDRANT_URL, QDRANT_API_KEY, QDRANT_COLLECTION
 
 client = QdrantClient(
     url=QDRANT_URL,
     api_key=QDRANT_API_KEY,
 )
-
 
 def ensure_collection():
     collections = client.get_collections().collections
@@ -20,10 +15,7 @@ def ensure_collection():
     if QDRANT_COLLECTION not in names:
         client.create_collection(
             collection_name=QDRANT_COLLECTION,
-            vectors_config=VectorParams(
-                size=1536,
-                distance=Distance.COSINE,
-            ),
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
         )
 
     return True
