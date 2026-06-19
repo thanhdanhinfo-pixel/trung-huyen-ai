9efaf45e949b5fd1e08bf97cf89917d9791a13fd
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-19 15:04:03 +0700

    Update vectordb.py

--- rag/vectordb.py
+++ rag/vectordb.py
@@ -1,8 +1,1 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="/tmp/chroma")
-
-collection = client.get_or_create_collection(
-    name="trung_huyen_knowledge"
-)
-print(collection.count())
+from vectordb import client, COLLECTION_NAME, ensure_collection
