7aa831c85613d29750ea193cdf4930ea39cbe4f5
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-19 23:34:07 +0700

    Update markdown.py

--- knowledge/markdown.py
+++ knowledge/markdown.py
@@ -1,1 +1,16 @@
+import re
+import unicodedata
 
+
+def safe_filename(name: str) -> str:
+    # Bỏ dấu tiếng Việt
+    name = unicodedata.normalize("NFKD", name)
+    name = name.encode("ascii", "ignore").decode("ascii")
+
+    # Xóa ký tự đặc biệt
+    name = re.sub(r"[^a-zA-Z0-9\s_-]", "", name)
+
+    # Đổi khoảng trắng thành _
+    name = "_".join(name.split())
+
+    return name.lower()
