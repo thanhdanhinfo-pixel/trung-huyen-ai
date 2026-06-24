a0ab54a8307107264ad1b996ae1e6a72bf5b5329
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-24 17:25:25 +0700

    Export observation observers

--- kernel/observation/__init__.py
+++ kernel/observation/__init__.py
@@ -6,6 +6,14 @@ SystemNode/SystemRelationship objects for the Kernel Digital Twin.
 
 from .observation_result import ObservationResult
 from .repository_observer import repository_observer
+from .runtime_observer import runtime_observer
+from .tool_observer import tool_observer
 from .system_observer import system_observer
 
-__all__ = ["ObservationResult", "repository_observer", "system_observer"]
+__all__ = [
+    "ObservationResult",
+    "repository_observer",
+    "runtime_observer",
+    "tool_observer",
+    "system_observer",
+]
