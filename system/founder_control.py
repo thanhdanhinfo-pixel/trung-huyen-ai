875d9041fa6c15399a789c4f52d705401b3eed8f
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:56:00 +0700

    GOVERNANCE-WAVE-6

--- system/founder_control.py
+++ system/founder_control.py
@@ -1,10 +1,2 @@
-PROTECTED_AREAS={'identity','mission','core_architecture','constitution'}
-
-class FounderControl:
-    def can_modify(self, area:str)->bool:
-        return area not in PROTECTED_AREAS
-
-    def review_required(self, area:str):
-        return area in PROTECTED_AREAS
-
-founder_control=FounderControl()
+# Compatibility shim. Preferred import: system.governance_pkg.founder
+from system.governance_pkg.founder import *
