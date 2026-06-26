5864fd7ae48e0a124cc342a8759aebd203d4d107
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:25:45 +0700

    Implement governance checks and auto boot on FastAPI startup

--- system/__init__.py
+++ system/__init__.py
@@ -1,9 +1,11 @@
 from .system_awareness import system_awareness
 from .digital_twin import digital_twin
 from .observability_layer import observability
+from .governance import governance
 
 __all__ = [
     'system_awareness',
     'digital_twin',
     'observability',
+    'governance',
 ]
