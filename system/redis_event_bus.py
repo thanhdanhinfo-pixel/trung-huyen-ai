84d61c5f9ff372340758fff06388e3245d758da5
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:55:13 +0700

    EVENTS-WAVE-5

--- system/redis_event_bus.py
+++ system/redis_event_bus.py
@@ -1,11 +1,2 @@
-import os
-
-class RedisEventBus:
-    def status(self):
-        return {
-            'enabled': os.getenv('EVENT_BACKEND') == 'redis',
-            'url': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
-            'mode': 'adapter-ready'
-        }
-
-redis_event_bus = RedisEventBus()
+# Compatibility shim. Preferred import: system.events.redis_adapter
+from system.events.redis_adapter import *
