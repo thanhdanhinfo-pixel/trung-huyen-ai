e13aabb990ba3136ab78a7df29ca0f6c28884843
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:56:17 +0700

    Close last scheduler issue

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ app = FastAPI(
 @app.on_event('startup')
 async def system_startup_boot():
     boot()
+    production_scheduler.start()
 
 app.add_middleware(
     CORSMiddleware,
@@ -102,8 +103,6 @@ from api.debug import router as debug_router
 from api.runtime import router as runtime_router, register_error
 from api.deployment import router as deployment_router
 from api.system_runtime import router as system_runtime_router
-from system.production_scheduler import production_scheduler
-from system.bootstrap import boot
 
 app.include_router(system_router)
 app.include_router(debug_router)
