30f64176ecaefadf2bece7a95caf02c571259c63
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-27 03:55:24 +0700

    PHASE-RUN-COMMAND-SANDBOX-FOUNDATION

---
+++ services/command_runner/allowlist.py
@@ -0,0 +1,12 @@
+ALLOWED_COMMANDS = {
+    'import_app': ['python', '-c', "import app; print('APP_IMPORT_OK')"],
+    'compileall': ['python', '-m', 'compileall', '.'],
+}
+
+
+def get_command(name: str):
+    return ALLOWED_COMMANDS.get(name)
+
+
+def list_allowed():
+    return sorted(ALLOWED_COMMANDS.keys())
