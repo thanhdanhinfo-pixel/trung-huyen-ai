3ab2f9e504b32eaa7edf78e9bc3d9ecc3e6e3067
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:57:22 +0700

    Advance remaining visualization and digital twin capabilities without overstating production status

--- app.py
+++ app.py
@@ -103,6 +103,8 @@ from api.debug import router as debug_router
 from api.runtime import router as runtime_router, register_error
 from api.deployment import router as deployment_router
 from api.system_runtime import router as system_runtime_router
+from api.digital_twin import router as digital_twin_router
+from api.graph import router as graph_router
 
 app.include_router(system_router)
 app.include_router(debug_router)
