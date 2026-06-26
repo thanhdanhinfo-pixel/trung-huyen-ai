52bc10408feeb82ebdd6b875bc645d8583263564
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:15:57 +0700

    STRUCTURE-CLEANUP-4B Capability Completion

---
+++ system/capability/dependencies.py
@@ -0,0 +1,7 @@
+class CapabilityDependencies:
+    def resolve(self, capability, depends_on=None):
+        deps=depends_on or []
+        return {'capability':capability,'depends_on':deps,'required_by':[],'has_cycle':capability in deps}
+    def topological_order(self, graph):
+        return list(graph.keys())
+capability_dependencies=CapabilityDependencies()
