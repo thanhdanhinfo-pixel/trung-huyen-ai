a56cfb769dedb5226245b331a22c72ff17f4fe0a
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:14:52 +0700

    STRUCTURE-CLEANUP-4 Capability Migration

--- system/capability/__init__.py
+++ system/capability/__init__.py
@@ -1,1 +1,7 @@
-# Capability model package.
+from .evolution import CapabilityEvolution, capability_evolution
+from .lineage import CapabilityLineage, capability_lineage
+
+__all__=[
+'CapabilityEvolution','capability_evolution',
+'CapabilityLineage','capability_lineage'
+]
