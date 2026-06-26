a528ca87de4855eb02d36d29f30f64690812b8e1
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:09:38 +0700

    STRUCTURE-CLEANUP-2B Event ACK Migration

--- system/event_ack.py
+++ system/event_ack.py
@@ -1,21 +1,2 @@
-from datetime import datetime
-from system.event_ack_store import event_ack_store
-
-class EventAck:
-    def __init__(self):
-        self._acks=[]
-
-    def acknowledge(self,event_id:str,consumer:str):
-        ack={
-            'event_id':event_id,
-            'consumer':consumer,
-            'acknowledged_at':datetime.utcnow().isoformat()+'Z'
-        }
-        self._acks.append(ack)
-        event_ack_store.append(ack)
-        return ack
-
-    def snapshot(self):
-        return {'count':len(self._acks),'acks':self._acks[-100:]}
-
-event_ack=EventAck()
+# Compatibility shim. Preferred import: system.events.ack
+from system.events.ack import *
