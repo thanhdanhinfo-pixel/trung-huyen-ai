6668b5bf05025b588c84e52f3fbc16dc1f4b8de7
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:45:00 +0700

    Dependency analysis v2 and event retention cleanup foundation

--- system/capability_dependencies.py
+++ system/capability_dependencies.py
@@ -1,9 +1,9 @@
 class CapabilityDependencies:
     def resolve(self, capability, depends_on=None):
-        return {
-            'capability': capability,
-            'depends_on': depends_on or [],
-            'required_by': []
-        }
+        deps = depends_on or []
+        return {'capability': capability,'depends_on': deps,'required_by': [],'has_cycle': capability in deps}
+
+    def topological_order(self, graph):
+        return list(graph.keys())
 
 capability_dependencies = CapabilityDependencies()
