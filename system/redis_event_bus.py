608365aaf3fa0df3acd4144ffa1ed5b7e4fac0b6
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:53:58 +0700

    Add dependencies and environment templates

---
+++ system/redis_event_bus.py
@@ -0,0 +1,11 @@
+import os
+
+class RedisEventBus:
+    def status(self):
+        return {
+            'enabled': os.getenv('EVENT_BACKEND') == 'redis',
+            'url': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
+            'mode': 'adapter-ready'
+        }
+
+redis_event_bus = RedisEventBus()
