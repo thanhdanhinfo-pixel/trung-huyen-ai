864160460b13c8584455677755c596a19133eae8
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:55:57 +0700

    GOVERNANCE-WAVE-6

---
+++ system/governance_pkg/founder.py
@@ -0,0 +1,5 @@
+PROTECTED_AREAS={'identity','mission','core_architecture','constitution'}
+class FounderControl:
+    def can_modify(self,area:str)->bool: return area not in PROTECTED_AREAS
+    def review_required(self,area:str): return area in PROTECTED_AREAS
+founder_control=FounderControl()
