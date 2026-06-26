ca76dc05f439bcdc337680778ce239a7a1037a9f
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 12:00:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
