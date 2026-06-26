96e5daa5f7bc9e336a488b9f0e461ad81da6b90a
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:13:36 +0700

    STRUCTURE-CLEANUP-3B Scheduler Workers Migration

---
+++ system/scheduler/evolution_worker.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from system.evolution_engine import evolution_engine
+class EvolutionWorker:
+    def run_once(self):
+        result=evolution_engine.evolve()
+        return {'time':datetime.utcnow().isoformat()+'Z','status':'completed','result':result}
+evolution_worker=EvolutionWorker()
