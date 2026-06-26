aadfa32cd63ac63d02538394581eb7c253f13b05
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:30:57 +0700

    Add policy engine after drift reconciliation

--- system/__init__.py
+++ system/__init__.py
@@ -2,10 +2,14 @@ from .system_awareness import system_awareness
 from .digital_twin import digital_twin
 from .observability_layer import observability
 from .governance import governance
+from .self_healing import self_healing
+from .policy_engine import policy_engine
 
 __all__ = [
     'system_awareness',
     'digital_twin',
     'observability',
     'governance',
+    'self_healing',
+    'policy_engine',
 ]
