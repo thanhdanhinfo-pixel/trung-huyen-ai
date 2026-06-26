1ab94f6ea88ab74be8d8cd8cc9564bff6e547106
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:17:55 +0700

    STRUCTURE-CLEANUP-5B Evolution Completion

--- system/evolution/__init__.py
+++ system/evolution/__init__.py
@@ -1,4 +1,6 @@
 from .reflection import ReflectionEngine, reflection_engine
 from .learning import LearningEngine, learning_engine
 
-__all__=['ReflectionEngine','reflection_engine','LearningEngine','learning_engine']
+from .adaptation import AdaptationEngine, adaptation_engine
+
+__all__=['ReflectionEngine','reflection_engine','LearningEngine','learning_engine','AdaptationEngine','adaptation_engine']
