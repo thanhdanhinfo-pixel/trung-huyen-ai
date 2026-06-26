e097b706f517f85530cfcfb8036fd25efe6774fb
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:04:07 +0700

    Reduce namespace risk and add health validation tooling

---
+++ tests/test_validation_contract.py
@@ -0,0 +1,7 @@
+from system.validation_report import validation_report
+
+def test_validation_contract():
+    data = validation_report.run()
+    assert 'build' in data
+    assert 'imports' in data
+    assert 'apis' in data
