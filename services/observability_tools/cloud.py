a227af7d31a48ada88a1900e03412a89b4e4c3f5
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-27 04:08:38 +0700

    PHASE-OBSERVABILITY-TOOLS-FOUNDATION

---
+++ services/observability_tools/cloud.py
@@ -0,0 +1,7 @@
+def cloud_status():
+    return {
+        'cloud_run_logs': 'pending_api_client',
+        'cloud_build_status': 'pending_api_client',
+        'cloud_run_revisions': 'pending_api_client',
+        'mode': 'foundation',
+    }
