b9cc91bdcfd555275782d8b580fb3aef581b258f
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:27:26 +0700

    REAL Rules Migration V2

--- system/rule_engine_v2.py
+++ system/rule_engine_v2.py
@@ -1,22 +1,2 @@
-from system import event_bus, self_healing
-
-class RuleEngineV2:
-    def evaluate_runtime(self, worker_failed=False, backlog=0, boot_failed=False):
-        actions=[]
-        if worker_failed:
-            actions.append('restart_worker')
-        if backlog>100:
-            actions.append('scale_workers')
-        if boot_failed:
-            actions.append('restore_last_healthy_snapshot')
-        return actions or ['system_nominal']
-
-    def execute(self, **kwargs):
-        actions=self.evaluate_runtime(**kwargs)
-        for a in actions:
-            event_bus.publish('RULE_V2_ACTION',{'action':a})
-            if a=='restore_last_healthy_snapshot':
-                self_healing.auto_repair()
-        return {'actions':actions}
-
-rule_engine_v2=RuleEngineV2()
+# Compatibility shim. Preferred import: system.rules.engine_v2
+from system.rules.engine_v2 import *
