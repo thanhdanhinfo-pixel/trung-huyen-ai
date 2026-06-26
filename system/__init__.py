df137f1c1fd89e172d192e9136d68949ed8e357e
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 22:23:15 +0700

    Add system package init and upgrade boot sequence for awareness stack

---
+++ system/__init__.py
@@ -0,0 +1,9 @@
+from .system_awareness import system_awareness
+from .digital_twin import digital_twin
+from .observability_layer import observability
+
+__all__ = [
+    'system_awareness',
+    'digital_twin',
+    'observability',
+]
