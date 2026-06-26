8014ce213c8756c369b6a51b253768ec5391428f
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:31:06 +0700

    REAL Runtime Migration

---
+++ system/runtime/task.py
@@ -0,0 +1,5 @@
+from task_queue import TaskQueue
+queue=TaskQueue()
+def task_status():
+    snapshot=queue.snapshot()
+    return {'pending_count':snapshot['pending_count'],'history_count':snapshot['history_count'],'pending':snapshot['pending'],'history':snapshot['history'][-10:]}
