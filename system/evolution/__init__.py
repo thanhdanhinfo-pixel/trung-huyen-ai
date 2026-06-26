0397e0d4d12de47b7cc3081c157c9430099e173c
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:17:13 +0700

    STRUCTURE-CLEANUP-5 Evolution Migration

--- system/evolution/__init__.py
+++ system/evolution/__init__.py
@@ -1,1 +1,4 @@
-# Learning and evolution package.
+from .reflection import ReflectionEngine, reflection_engine
+from .learning import LearningEngine, learning_engine
+
+__all__=['ReflectionEngine','reflection_engine','LearningEngine','learning_engine']
