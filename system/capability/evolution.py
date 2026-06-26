3b207453e5649a7d7ad74cb1f6e20cd4c36fbd71
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:14:46 +0700

    STRUCTURE-CLEANUP-4 Capability Migration

---
+++ system/capability/evolution.py
@@ -0,0 +1,9 @@
+CAPABILITY_STATES=['PROPOSED','EXPERIMENTAL','ACTIVE','DEPRECATED','REMOVED']
+class CapabilityEvolution:
+    def promote(self,name,current):
+        try:
+            idx=CAPABILITY_STATES.index(current)
+            return CAPABILITY_STATES[min(idx+1,len(CAPABILITY_STATES)-1)]
+        except ValueError:
+            return 'PROPOSED'
+capability_evolution=CapabilityEvolution()
