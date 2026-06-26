ba1c687b1cdf601019bd39e137ea7cee5e5c8800
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:01:01 +0700

    Add stable facade modules without deleting existing implementations

---
+++ system/evolution_system.py
@@ -0,0 +1,7 @@
+from system.reflection_engine import *
+from system.learning_engine import *
+from system.adaptation_engine import *
+from system.evolution_engine import *
+from system.evolution_history import *
+
+# Stable public facade for learning/evolution lifecycle.
