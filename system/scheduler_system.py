83893c070db9ab151de010bb9035e6766041247b
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:00:57 +0700

    Add stable facade modules without deleting existing implementations

---
+++ system/scheduler_system.py
@@ -0,0 +1,7 @@
+from system.production_scheduler import *
+from system.scheduler_runtime import *
+from system.evolution_worker import *
+from system.retention_worker import *
+from system.worker_supervision import *
+
+# Stable public facade for scheduler/workers.
