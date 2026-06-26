2e4f83e4af01f95c12cc76da8fe5a9be23aca66a
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-26 23:16:03 +0700

    STRUCTURE-CLEANUP-4B Capability Completion

--- system/capability/__init__.py
+++ system/capability/__init__.py
@@ -1,7 +1,11 @@
 from .evolution import CapabilityEvolution, capability_evolution
 from .lineage import CapabilityLineage, capability_lineage
+from .dependencies import CapabilityDependencies, capability_dependencies
+from .risk import DependencyRisk, dependency_risk
 
 __all__=[
 'CapabilityEvolution','capability_evolution',
-'CapabilityLineage','capability_lineage'
+'CapabilityLineage','capability_lineage',
+'CapabilityDependencies','capability_dependencies',
+'DependencyRisk','dependency_risk'
 ]
