93d6b391e93e247ce417eb30718a08bc0f8c7c76
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-28 20:56:15 +0700

    chore(developer): remove placeholder marker for write safety gate

--- services/developer_runtime.py
+++ services/developer_runtime.py
@@ -43,7 +43,7 @@ class DeveloperRuntime:
                 "patch_files",
                 "batch_operations",
                 "verify_paths",
-                "rollback_placeholder",
+                "rollback_v2_planned",
             ],
         }
 
