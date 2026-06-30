eb680921b399ac2e328fabe5dad04679bbf95aff
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 00:43:27 +0700

    Update capability_system.py

--- system/capability_system.py
+++ system/capability_system.py
@@ -1,6 +1,6 @@
 from system.capability_evolution import *
 from system.capability_lineage import *
-from system.capability_dependencies import *
+from system.capability_dependencies import * 
 from system.dependency_risk import * 
 
 # Stable public facade for capability metadata.
