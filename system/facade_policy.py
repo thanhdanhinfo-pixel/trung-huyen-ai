09e373aac30f069230839a7feea084d2dfcacd82
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 00:32:51 +0700

    Update facade_policy.py

--- system/facade_policy.py
+++ system/facade_policy.py
@@ -1,2 +1,2 @@
 # Compatibility facade registry
-FACADE_MODULES=['system.event_system','system.scheduler_system','system.capability_system','system.evolution_system']
+FACADE_MODULES=['system.event_system','system.scheduler_system','system.capability_system','system.evolution_system'] 
