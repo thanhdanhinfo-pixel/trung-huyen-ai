02b9482daaf5c527f3fa53a4e18c4cdcba75ea25
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 01:29:44 +0700

    Update self_improvement.py

--- self_improvement.py
+++ self_improvement.py
@@ -1,4 +1,4 @@
-class SelfImprovement:
+class SelfImprovement: 
     def suggest(self, report):
         if report.get("needs_improvement"):
             return ["Bổ sung ngữ cảnh","Tăng số nguồn tham chiếu"]
