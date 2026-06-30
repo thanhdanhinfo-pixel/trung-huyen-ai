fcf0a8b8807cf02bc29700fc21d7fa53c36c38a6
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-30 23:04:36 +0700

    Update redis_adapter.py

--- system/events/redis_adapter.py
+++ system/events/redis_adapter.py
@@ -1,5 +1,5 @@
 import os
-class RedisEventBus:
+class RedisEventBus: 
     def status(self):
         return {'enabled':os.getenv('EVENT_BACKEND')=='redis','url':os.getenv('REDIS_URL','redis://localhost:6379/0'),'mode':'adapter-ready'}
 redis_event_bus=RedisEventBus() 
