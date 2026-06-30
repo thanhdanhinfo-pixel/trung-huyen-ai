e03abdbe0ecf24f338b12143c8a6e43155192fd8
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-30 22:56:33 +0700

    Update ws.py

--- system/events/ws.py
+++ system/events/ws.py
@@ -2,3 +2,4 @@ class WsSubscriptions:
     def match(self,subs,event_type):
         return any(s.endswith('*') and event_type.startswith(s[:-1]) or s==event_type for s in subs)
 ws_subscriptions=WsSubscriptions()
+ 
