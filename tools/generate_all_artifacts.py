becf2072f67164477ccc69b8b62ce366e07524eb
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 01:39:17 +0700

    Update generate_all_artifacts.py

--- tools/generate_all_artifacts.py
+++ tools/generate_all_artifacts.py
@@ -2,5 +2,5 @@ import subprocess,sys
 TOOLS=['repo_snapshot.py','dependency_graph.py','root_audit.py','orphan_detector.py','repo_tree.py','architecture_audit.py','move_recommendation.py','import_safety.py']
 for t in TOOLS:
     print(f'RUN {t}')
-    subprocess.call([sys.executable,f'tools/{t}'])
+    subprocess.call([sys.executable,f'tools/{t}']) 
 print('ALL ARTIFACTS GENERATED')
