444bfab48a28b03feac0fe02682cde0e47d1808b
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 01:07:14 +0700

    Update change_detector.py

--- change_detector.py
+++ change_detector.py
@@ -1,3 +1,3 @@
-def detect_changes(old_index:list,new_index:list):
+def detect_changes(old_index:list,new_index:list): 
     old_ids={d["id"] for d in old_index}
     return [d for d in new_index if d["id"] not in old_ids]
