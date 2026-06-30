22977c6b2eae647750f783713cb8c1fecf9b06c7
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-01 00:33:15 +0700

    Update evolution_system.py

--- system/evolution_system.py
+++ system/evolution_system.py
@@ -1,4 +1,4 @@
-from system.reflection_engine import *
+from system.reflection_engine import * 
 from system.learning_engine import *
 from system.adaptation_engine import *
 from system.evolution_engine import *
