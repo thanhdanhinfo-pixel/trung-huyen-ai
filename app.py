e645fa87b47f32a3ba9105b94237cbfa040b96e8
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-06-30 10:23:02 +0700

    system write patch

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import asyncio
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
