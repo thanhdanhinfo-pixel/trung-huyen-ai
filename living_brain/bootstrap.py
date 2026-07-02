8e30f3a79779d18a258b6b87fddb7204fb402b4c
thanhdanhinfo-pixel <thanhdanh.info@gmail.com>
2026-07-02 09:59:36 +0700

    Wire bootstrap to tool router

--- living_brain/bootstrap.py
+++ living_brain/bootstrap.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import yaml
 from living_brain.facade import living_brain
 from living_brain.tool_router import tool_router, ToolRoutingError
+from living_brain.tool_router import tool_router, ToolRoutingError
 
 
 def bootstrap_brain() -> dict:
